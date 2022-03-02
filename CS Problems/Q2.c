c229a4bbfe52383b6f871876607e86474b9aec80
54nd339 <94390sandeep@gmail.com>
2022-03-03 01:32:00 +0530

    commit

--- CS Problems/Q2.c
+++ CS Problems/Q2.c
@@ -6,23 +6,25 @@
 pthread_mutex_t mutex;  //to make sure 1 player at a time
 sem_t *S; int n;        //n semaphores for n players
 
+int next(int pid, int val) {
+    int next;
+    if(val!=6) {
+        next = (pid + 1) % n;
+        printf("Player %d passed the die to %d\n", pid + 1, next + 1);
+    }
+    else next = pid;
+    return next;
+}
 void *player() {
     int pid = 0;
 	do {
         pthread_mutex_lock(&mutex);     //Enters CS
-        six:
+        sem_post(&S[pid]);              //signals player is going to roll
         int val = (rand() % 6) + 1;
         printf("Player %d rolled and got %d\n", pid + 1, val);
-        if(val == 6) goto six;
-        sem_post(&S[pid]);              //signals player has rolled the die
         pthread_mutex_unlock(&mutex);   //exits CS
         sem_wait(&S[pid]);              //player waits for the next turn
-        pthread_mutex_lock(&mutex);     //Enters CS
-        int next = (pid + 1) % n;
-        printf("Player %d passed the die to %d\n", pid + 1, next + 1);
-        sem_post(&S[next]);             //wakes up next member
-        pthread_mutex_unlock(&mutex);   //exits CS
-        pid = next;
+        pid = next (pid, val);
 	} while(1);
 }
 
